e8a67626d3275596b2c7880be180015e0f53fcaa
Mohammad Hossain <moh.hossain98@gmail.com>
2021-01-02 01:43:24 -0500

    final commit all passing cases

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,17 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+      let middle = Math.floor((nums.length / 2))
+      if (nums.length === 1 && nums[0] !== target) {
+          return false;
+      }
+      if (nums[middle] === target) {
+          return true
+      } else if (target > nums[middle]) {
+          return this.binarySearch(nums.slice(middle), target)
+      } else if (target < nums[middle]) {
+          return this.binarySearch(nums.slice(0, middle), target)
+      }
   }
 }
 
